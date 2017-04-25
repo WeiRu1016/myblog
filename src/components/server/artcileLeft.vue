ccf32598dedceb5ee10f997ad245667f0baba469
weiru <weiru@rd.tuan800.com>
2017-04-25 15:59:18 +0800

    后台编辑

---
+++ src/components/server/artcileLeft.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="article-left" :style="{width: width,height: height}">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'leftBar',
+    props: {
+      width: {
+        type: String,
+        default: 'auto',
+        require: false
+      },
+      height: {
+        type: String,
+        default: '100%',
+        require: false
+      }
+    }
+  }
+</script>
+<style lang="scss" scopedSlots>
+  #article-left{
+    background: #ffffff;
+    min-width: 200px;
+  }  
+</style>
---
+++ src/components/server/leftBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="leftBar" :style="{width: width,height: height}">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'leftBar',
+    props: {
+      width: {
+        type: String,
+        default: 'auto',
+        require: false
+      },
+      height: {
+        type: String,
+        default: '100%',
+        require: false
+      }
+    }
+  }
+</script>
+<style lang="scss" scopedSlots>
+  #leftBar{
+    background: #3f3f3f;
+    min-width: 200px;
+  }  
+</style>
