27589d5191f1890278544282a4f971dfba2295de
weiru <weiru@zhuanzhuan.com>
2018-03-19 11:06:30 +0800

    修改端口

--- server/routes/all.js
+++ server/routes/all.js
@@ -1,5 +1,8 @@
 module.exports = function (app) {
   // app.use('/api/login', loginPlugin.isNotLogin, require('./login'));
+  app.use('/api/test', function (req, res, next) {
+    res.send('success~.~ baomax');
+  });
   app.use('/api/login', require('./login'));
   app.use('/api/article', require('./article'));
   app.use('/api/catagory', require('./catagory'));
